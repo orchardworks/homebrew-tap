5e87dc5dd43afbcb597b901e99d0f090b3c4f8c8
Clementine Park <clementine@orchardworks.dev>
2026-05-18 11:07:55 +0900

    Switch orchard-ops cask URL to dist.orchard-ops.com (R2)

--- Casks/orchard-ops.rb
+++ Casks/orchard-ops.rb
@@ -2,7 +2,7 @@ cask "orchard-ops" do
   version "1.0.1"
   sha256 "da7b2d80b0ac12f024aa30b57363f44edca023825e6d9e467c1ad13e2cdf8c7c"
 
-  url "https://github.com/orchardworks/orchard-ops.com/releases/download/v#{version}/OrchardOps.dmg"
+  url "https://dist.orchard-ops.com/v#{version}/OrchardOps.dmg"
   name "Orchard Ops"
   desc "Schedule scripts and commands on your Mac with a visual timeline"
   homepage "https://orchard-ops.com/"
